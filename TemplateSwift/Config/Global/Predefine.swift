3be55af36bc8bd5ba11932028b69776841127273
jiasong <593908937@qq.com>
2019-10-14 18:44:47 +0800

    Test

---
+++ TemplateSwift/Config/Global/Predefine.swift
@@ -0,0 +1,16 @@
+//
+//  Predefine.swift
+//  TemplateSwift
+//
+//  Created by RuanMei on 2019/10/14.
+//  Copyright © 2019 RuanMei. All rights reserved.
+//
+
+import UIKit
+
+let scrent = 375;
+
+func zzz() -> Void {
+    let font:UIFont =  UIFont.systemFont(ofSize: 12)
+    print(font)
+}
